50e32eaaf689c4c0c944f41c1a75d95c6fdad28d
Steven Wanderski <steven.wanderski@gmail.com>
2018-07-16 07:22:24 -0500

    Adds https://www to default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'coaxandfolks.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.coaxandfolks.com' }
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
